467bdedd8530c4dd809cbf8f24138c7aa4c8a0a8
Anna Lisitsyna <anna.lisitsyna93@gmail.com>
2024-05-16 22:30:07 +0300

    fix: fix key value

--- src/app/(admin)/dashboard/@categories/loading.tsx
+++ src/app/(admin)/dashboard/@categories/loading.tsx
@@ -1,12 +1,11 @@
-import Skeleton from '@/app/components/Skeleton';
 import { StatCardType } from '@/app/components/StatCard/StatCard';
 import StatCardSkeleton from '@/app/components/StatCard/StatCardSkeleton';
 
 const LoadingCategories = () => (
   <div className="pb-5 px-5 pt-[68px] rounded animate-pulse w-full h-full bg-gray-200">
     <div className="flex gap-3 flex-wrap w-full">
-      {Array.from({ length: 8 }).map((item) => (
-        <StatCardSkeleton key={item as number} type={StatCardType.Dark} />
+      {Array.from({ length: 8 }).map((item, index) => (
+        <StatCardSkeleton key={`${item}-${index}`} type={StatCardType.Dark} />
       ))}
     </div>
   </div>
--- src/app/(admin)/dashboard/@stats/loading.tsx
+++ src/app/(admin)/dashboard/@stats/loading.tsx
@@ -3,8 +3,8 @@ import StatCardSkeleton from '@/app/components/StatCard/StatCardSkeleton';
 
 const StatsLoading = () => (
   <div className="w-full flex gap-5">
-    {Array.from({ length: 4 }).map((item) => (
-      <StatCardSkeleton key={item as number} type={StatCardType.Gradient} />
+    {Array.from({ length: 4 }).map((item, index) => (
+      <StatCardSkeleton key={`${index}-${item}`} type={StatCardType.Gradient} />
     ))}
   </div>
 );
