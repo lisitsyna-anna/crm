af79072aed875e01685d00888c6569cd7df01e5a
Anna Lisitsyna <anna.lisitsyna93@gmail.com>
2024-05-19 19:52:02 +0300

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
-import './globals.css';
 import { Plus_Jakarta_Sans } from 'next/font/google';
+import { Metadata } from 'next';
 import QueryProvider from './providers/QueryProvider';
+
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'TruScape',
+  icons: {
+    icon: 'icon.ico',
+  },
+};
 
 const font = Plus_Jakarta_Sans({ subsets: ['latin'] });
 
