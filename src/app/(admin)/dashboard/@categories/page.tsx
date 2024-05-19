c4f58e896c41b03d4b9129d12779f3d5626d21e2
Anna Lisitsyna <anna.lisitsyna93@gmail.com>
2024-05-19 22:14:23 +0300

    fix: fix prod error

--- src/app/(admin)/dashboard/@categories/page.tsx
+++ src/app/(admin)/dashboard/@categories/page.tsx
@@ -3,38 +3,29 @@ import StatCard, { StatCardType } from '@/app/components/StatCard/StatCard';
 import { getCategories, getCompanies } from '@/lib/api';
 import { getCountById } from '@/lib/utils/getCountById';
 
+export const dynamic = 'force-dynamic';
+
 const CategoriesPage = async () => {
-  try {
-    console.log('Fetching categories');
-    const categories = await getCategories();
-    console.log('Fetched categories:', categories);
+  const categories = await getCategories();
+  const companies = await getCompanies();
 
-    console.log('Fetching companies');
-    const companies = await getCompanies();
-    console.log('Fetched companies:', companies);
+  const counts = getCountById(companies, 'categoryId');
 
-    const counts = getCountById(companies, 'categoryId');
-
-    return (
-      <DashboardCard label="Categories of companies">
-        <div className="grid grid-cols-12 gap-3 pb-5 px-5">
-          {categories.map(({ id, title }, index) => (
-            <div key={id} className="col-span-3">
-              <StatCard
-                type={StatCardType.Dark}
-                label={title}
-                counter={counts[id] || 0}
-                isTextLime={(index + 1) % 2 === 0}
-              />
-            </div>
-          ))}
-        </div>
-      </DashboardCard>
-    );
-  } catch (error) {
-    console.error('Error in CategoriesPage:', error);
-    return <div>Error loading categories</div>;
-  }
+  return (
+    <DashboardCard label="Categories of companies">
+      <div className="grid grid-cols-12 gap-3 pb-5 px-5">
+        {categories.map(({ id, title }) => (
+          <div key={id} className="col-span-3">
+            <StatCard
+              type={StatCardType.Dark}
+              label={title}
+              counter={counts[id] || 0}
+            />
+          </div>
+        ))}
+      </div>
+    </DashboardCard>
+  );
 };
 
 export default CategoriesPage;
