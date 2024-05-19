08989a896290dbd4a403aa6fbe9532b5ff164d25
Anna Lisitsyna <anna.lisitsyna93@gmail.com>
2024-05-19 21:29:32 +0300

    test prod

--- src/app/(admin)/dashboard/@categories/page.tsx
+++ src/app/(admin)/dashboard/@categories/page.tsx
@@ -4,30 +4,31 @@ import { getCategories, getCompanies } from '@/lib/api';
 import { getCountById } from '@/lib/utils/getCountById';
 
 const CategoriesPage = async () => {
-  const categories = await getCategories();
-  const companies = await getCompanies();
+  // const categories = await getCategories();
+  // const companies = await getCompanies();
 
-  const counts = getCountById(companies, 'categoryId');
+  // const counts = getCountById(companies, 'categoryId');
 
-  console.log('categories', categories);
-  console.log('companies', companies);
-  console.log('counts', counts);
+  // console.log('categories', categories);
+  // console.log('companies', companies);
+  // console.log('counts', counts);
 
   return (
-    <DashboardCard label="Categories of companies">
-      <div className="grid grid-cols-12 gap-3 pb-5 px-5">
-        {categories.map(({ id, title }, index) => (
-          <div key={id} className="col-span-3">
-            <StatCard
-              type={StatCardType.Dark}
-              label={title}
-              counter={10}
-              isTextLime={(index + 1) % 2 === 0}
-            />
-          </div>
-        ))}
-      </div>
-    </DashboardCard>
+    <div>Categories</div>
+    // <DashboardCard label="Categories of companies">
+    //   <div className="grid grid-cols-12 gap-3 pb-5 px-5">
+    //     {categories.map(({ id, title }, index) => (
+    //       <div key={id} className="col-span-3">
+    //         <StatCard
+    //           type={StatCardType.Dark}
+    //           label={title}
+    //           counter={10}
+    //           isTextLime={(index + 1) % 2 === 0}
+    //         />
+    //       </div>
+    //     ))}
+    //   </div>
+    // </DashboardCard>
   );
 };
 
